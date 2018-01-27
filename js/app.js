b54e22a4dc40d6003e5eeb46a88093a0fb349afc
bikowalczyk <bikowalczyk@gmail.com>
2018-01-27 22:38:21 +0100

    v1.0

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
-const button =  $("input[type='submit']");
-
-  
+const button =  $('.link');  
 
 $(button).click(function(e){
     e.preventDefault();
 
-const link = $("input[type='url']").val();
+let link = $("input[type='url']").val();
 
-        if(link.startsWith("https://www.instagram.com/p/")){
+        if(link.startsWith("https://www.instagram.com/p/" || link.startsWith("https://scontent"))){
            const embed = "https://api.instagram.com/oembed?url=" + link;
            
            $.ajax({
@@ -16,8 +14,15 @@ const link = $("input[type='url']").val();
                cache: false,
                dataType: 'jsonp',
                success: function(data){
-                   console.log(data.thumbnail_url);
-                   
+                console.log(data.thumbnail_url);
+                button.attr('href', data.thumbnail_url);
+                button.attr('target', "_blank");
+                button.off();
+                button[0].click();
+                
+                
+                
+                 location.reload(false); //resets app 
                }
 
            }
@@ -29,4 +34,13 @@ const link = $("input[type='url']").val();
         else{               //the error message if not valid link
             
         alert("The link you pasted doesn't seem to be a valid instagram post link");
-        }});
+        }
+
+    });
+
+
+//ajax animation:
+$(document).on({
+    ajaxStart: function() { $('body').addClass("loading");},
+     ajaxStop: function() { $('body').removeClass("loading"); }    
+});
