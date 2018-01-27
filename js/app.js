a169f85af58b89e9aab71d479590b7eafb5a017b
bikowalczyk <bikowalczyk@gmail.com>
2018-01-27 14:54:01 +0100

    returns valid link in console

--- js/app.js
+++ js/app.js
@@ -2,22 +2,31 @@ const button =  $("input[type='submit']");
 
   
 
-$(button).click(function(){
+$(button).click(function(e){
+    e.preventDefault();
+
 const link = $("input[type='url']").val();
 
-async function scrape(){
-    console.log('pre');
- const wbpage = await $.get(link);
-   console.log(wbpage);
-   
-    }
-
         if(link.startsWith("https://www.instagram.com/p/")){
-            scrape();
-        } 
+           const embed = "https://api.instagram.com/oembed?url=" + link;
+           
+           $.ajax({
+               type: 'GET',
+               url: embed,
+               cache: false,
+               dataType: 'jsonp',
+               success: function(data){
+                   console.log(data.thumbnail_url);
+                   
+               }
+
+           }
+        
+        );
+
+        }
         
         else{               //the error message if not valid link
             
         alert("The link you pasted doesn't seem to be a valid instagram post link");
-        }
-});
+        }});
