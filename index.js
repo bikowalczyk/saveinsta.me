3333aeba9f21c062fa7754c99128282e1e9f0784
bikowalczyk <bikowalczyk@gmail.com>
2018-04-09 20:38:51 +0200

    beautified

--- index.js
+++ index.js
@@ -10,43 +10,43 @@ var path = require('path');
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/down', function(req, res){
-    const link = (JSON.stringify(req.query.url).slice(1,-1));   //direct mp4/jpg link
+app.get('/down', function (req, res) {
+  const link = (JSON.stringify(req.query.url).slice(1, -1)); //direct mp4/jpg link
 
-    //now download file locally, send download url and destroy file
-    const fileName = link.slice(-14);  
-    const r = request(link);
+  //now download file locally, send download url and destroy file
+  const fileName = link.slice(-14);
+  const r = request(link);
 
-    r.on('response', (resp)=>{
+  r.on('response', (resp) => {
     const stream = resp.pipe(fs.createWriteStream(__dirname + '/downloads/' + fileName));
-    stream.on('finish', ()=>res.download(__dirname + '/downloads/' + fileName,(err)=>{
-      fs.unlink(__dirname + '/downloads/' + fileName,(err)=>{
-        if (err){
+    stream.on('finish', () => res.download(__dirname + '/downloads/' + fileName, (err) => {
+      fs.unlink(__dirname + '/downloads/' + fileName, (err) => {
+        if (err) {
           throw err;
         }
       });
     }));
-    }); 
-  })
+  });
+})
 
-app.get('/gif', function(req, res){
-    const link = (JSON.stringify(req.query.url).slice(1,-1));   //direct mp4 link
+app.get('/gif', function (req, res) {
+  const link = (JSON.stringify(req.query.url).slice(1, -1)); //direct mp4 link
 
-    //now download file locally, send download url and destroy file
-    const fileName = link.slice(-14);  
-    const r = request(link);
+  //now download file locally, send download url and destroy file
+  const fileName = link.slice(-14);
+  const r = request(link);
 
-    r.on('response', (resp)=>{
+  r.on('response', (resp) => {
     const stream = resp.pipe(fs.createWriteStream(__dirname + '/downloads/' + fileName));
-    stream.on('finish',()=> gifify(__dirname + '/downloads/' + fileName).pipe(__dirname + '/downloads/' + fileName.slice(1,-4) +'.gif'));
-    }); 
+    stream.on('finish', () => gifify(__dirname + '/downloads/' + fileName).pipe(__dirname + '/downloads/' + fileName.slice(1, -4) + '.gif'));
+  });
 
 })
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.sendFile(__dirname + '/public/index.html');
 })
 
-app.listen(5000,'127.0.0.1', function() {
+app.listen(5000, '127.0.0.1', function () {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
--- public/js/app.js
+++ public/js/app.js
@@ -1,68 +1,67 @@
-const button =  $('.form__btn');
+const button = $('.form__btn');
 
 //binding enter press to clicking a button
-$('.form__input').keypress(function(e) {
-    if(e.which == 13) {
+$('.form__input').keypress(function (e) {
+    if (e.which == 13) {
         e.preventDefault();
         button[0].click();
     }
 });
 
-function download(value, item, type){
+function download(value, item, type) {
     $(item).attr('href', `${type}?url=${value}`);
     $(".form__input").val("");
     // button[0].click();
     // $(item).attr('href', "#");
     $('.form__input').prop('disabled', false);
-    
-   }
 
-$(button).click(function(e){
+}
+
+$(button).click(function (e) {
     let link = $(".form__input").val();
 
 
-        if(link.startsWith("https://www.instagram.com/p/") || link.startsWith("https://scontent")){
-            $('.form__input').prop('disabled', true);
-            $.ajax({
-                type: 'GET',
-                url: link,
-                cache: false,
-                success: function(response){
-                 const regex = /<meta property="og:video".*?content="(.*?)"/;       //a bit of a hassle with correct RegExp
-                 const regex1 = /<meta property="og:image".*?content="(.*?)"/;
-                 
-                if(regex.test(response)){
+    if (link.startsWith("https://www.instagram.com/p/") || link.startsWith("https://scontent")) {
+        $('.form__input').prop('disabled', true);
+        $.ajax({
+            type: 'GET',
+            url: link,
+            cache: false,
+            success: function (response) {
+                const regex = /<meta property="og:video".*?content="(.*?)"/; //a bit of a hassle with correct RegExp
+                const regex1 = /<meta property="og:image".*?content="(.*?)"/;
+
+                if (regex.test(response)) {
                     let src = response.match(regex)[1];
                     $(".form__btn").hide();
-                    $(".download__choice").css('display','flex');
-                    
+                    $(".download__choice").css('display', 'flex');
+
                     //user chose video
-                        $('.download__choice-video').click(()=>{
-                            download(encodeURIComponent(src), ('#video'),'/down');
-                            $(".download__choice").hide();
-                            $(".form__btn").css('display','block');
+                    $('.download__choice-video').click(() => {
+                        download(encodeURIComponent(src), ('#video'), '/down');
+                        $(".download__choice").hide();
+                        $(".form__btn").css('display', 'block');
                     });
                     //user chose gif
-                        $('.download__choice-gif').click(()=>{ 
-                            download(encodeURIComponent(src), ('#gif'),'/gif');
-                        });
+                    $('.download__choice-gif').click(() => {
+                        download(encodeURIComponent(src), ('#gif'), '/gif');
+                    });
 
-                }else{
+                } else {
                     let src = response.match(regex1)[1];
                     console.log(src);
-                    download(encodeURIComponent(src), ('.form__btn'),'/down');
+                    download(encodeURIComponent(src), ('.form__btn'), '/down');
 
                 }
 
-                }});
-                
-        }
-        
-        else{               //the error message if link isn't valid
-            $(".form__input").val("")
-            $(".form__input").addClass("animated flash");
-            setTimeout(()=>{ 
+            }
+        });
+
+    } else { //the error message if link isn't valid
+        $(".form__input").val("")
+        $(".form__input").addClass("animated flash");
+        setTimeout(() => {
             $("input").removeClass("animated flash");
-             }, 1500);
-        }
-    });
+        }, 1500);
+    }
+});
