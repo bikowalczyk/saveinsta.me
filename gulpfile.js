6083c536c8f3193a36194e4efc841c2475835f2d
bikowalczyk <bikowalczyk@gmail.com>
2018-04-16 22:35:53 +0200

    service worker and gulp added

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+let gulp = require("gulp");
+let rename = require("gulp-rename");
+let uglify = require('gulp-uglify-es').default;
+ 
+gulp.task("uglify", function () {
+    return gulp.src("public/js/*.js")
+        .pipe(rename("public/js/app-min.js"))
+        .pipe(uglify(/* options */))
+        .pipe(gulp.dest("./"));
+});
